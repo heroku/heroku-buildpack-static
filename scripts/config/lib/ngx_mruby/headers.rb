d700cdb0727dcaba011d18229cef84c15f6d93fe
Terence Lee <hone02@gmail.com>
2015-06-26 12:07:11 -0400

    use ngx_mruby to implement custom headers

---
+++ scripts/config/lib/ngx_mruby/headers.rb
@@ -0,0 +1,19 @@
+# ghetto require, since mruby doesn't have require
+eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
+
+USER_CONFIG = "/app/static.json"
+
+config = JSON.parse(File.read(USER_CONFIG))
+req    = Nginx::Request.new
+uri    = req.var.uri
+
+if config["headers"]
+  config["headers"].each do |route, header_hash|
+    if Regexp.compile("^#{NginxConfigUtil.to_regex(route)}$") =~ uri
+      header_hash.each do |key, value|
+        # value must be a string
+        req.headers_out[key] = value.to_s
+      end
+    end
+  end
+end
