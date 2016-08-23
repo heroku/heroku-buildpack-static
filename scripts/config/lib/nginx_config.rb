555b819cb935e2c54eae1a94594e1324931ebfd1
Randall A. Gordon <randall@randallagordon.com>
2016-08-23 11:21:39 -0700

    Allow username and password to be passed in via env vars

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -9,6 +9,7 @@ class NginxConfig
     clean_urls: false,
     https_only: false,
     basic_auth: false,
+    basic_auth_htpasswd_path: "/app/.htpasswd",
     worker_connections: 512
   }
 
@@ -34,7 +35,9 @@ class NginxConfig
 
     json["clean_urls"] ||= DEFAULT[:clean_urls]
     json["https_only"] ||= DEFAULT[:https_only]
+
     json["basic_auth"] ||= DEFAULT[:basic_auth]
+    json["basic_auth_htpasswd_path"] ||= DEFAULT[:basic_auth_htpasswd_path]
 
     json["routes"] ||= {}
     json["routes"] = NginxConfigUtil.parse_routes(json["routes"])
