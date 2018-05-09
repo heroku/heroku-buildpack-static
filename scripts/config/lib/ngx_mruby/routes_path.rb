934d13fa1febba25d557e74be00efa17649fd04b
Stéphane Bruckert <contact@stephanebruckert.com>
2018-05-09 16:58:25 +0100

    Proxy only when header matches

--- scripts/config/lib/ngx_mruby/routes_path.rb
+++ scripts/config/lib/ngx_mruby/routes_path.rb
@@ -3,16 +3,17 @@ eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
 
 USER_CONFIG = "/app/static.json"
 
-config      = {}
-config      = JSON.parse(File.read(USER_CONFIG)) if File.exist?(USER_CONFIG)
-req         = Nginx::Request.new
-uri         = req.var.uri
-nginx_route = req.var.route
-routes      = NginxConfigUtil.parse_routes(config["routes"])
-proxies     = config["proxies"] || {}
-redirects   = config["redirects"] || {}
+config       = {}
+config       = JSON.parse(File.read(USER_CONFIG)) if File.exist?(USER_CONFIG)
+req          = Nginx::Request.new
+uri          = req.var.uri
+nginx_route  = req.var.route
+routes       = NginxConfigUtil.parse_routes(config["routes"])
+proxies      = config["proxies"] || {}
+redirects    = config["redirects"] || {}
+should_proxy = NginxConfigUtil.should_proxy(config["accept"])
 
-if NginxConfigUtil.match_proxies(proxies.keys, uri) || NginxConfigUtil.match_redirects(redirects.keys, uri)
+if NginxConfigUtil.match_proxies(proxies.keys, uri) && should_proxy || NginxConfigUtil.match_redirects(redirects.keys, uri)
   # this will always fail, so try_files uses the callback
   uri
 else
