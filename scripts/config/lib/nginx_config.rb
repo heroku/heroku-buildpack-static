16063ebbd61f9b2f5a0797d16773b317194f7ced
Sean Bird <sean@neiybor.com>
2019-09-23 13:23:47 -0600

    add rendertron v2 ab test

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -24,6 +24,7 @@ class NginxConfig
     json['root'] ||= DEFAULT[:root]
     json['encoding'] ||= DEFAULT[:encoding]
     json['rendertron_api_base'] ||= ENV['RENDERTRON_API_BASE']
+    json['rendertron2_api_base'] ||= ENV['RENDERTRON2_API_BASE']
     json['environment'] ||= ENV['ENVIRONMENT']
     json['redirect_target'] ||= ENV['REACT_APP_REDIRECT_TARGET']
 
