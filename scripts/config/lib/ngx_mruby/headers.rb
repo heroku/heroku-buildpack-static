1fc438adf9e6aa5c706fee613ff999b460904f85
Russ <russ.developer@gmail.com>
2020-03-20 10:41:30 +0200

    ENVs enabled in headers

--- scripts/config/lib/ngx_mruby/headers.rb
+++ scripts/config/lib/ngx_mruby/headers.rb
@@ -2,18 +2,21 @@
 eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
 
 USER_CONFIG = "/app/static.json"
+ENV_CONFIG  = "/app/config/.env.json"
 
+envs   = {}
 config = {}
 config = JSON.parse(File.read(USER_CONFIG)) if File.exist?(USER_CONFIG)
+envs   = JSON.parse(File.read(ENV_CONFIG)) if File.exist?(ENV_CONFIG)
 req    = Nginx::Request.new
 uri    = req.var.uri
 
 if config["headers"]
   config["headers"].to_a.reverse.each do |route, header_hash|
-    if Regexp.compile("^#{NginxConfigUtil.to_regex(route)}$") =~ uri
+    if Regexp.compile("^#{NginxConfigUtil.to_regex(NginxConfigUtil.interpolate(route.dup, envs))}$") =~ uri
       header_hash.each do |key, value|
         # value must be a string
-        req.headers_out[key] = value.to_s
+        req.headers_out[NginxConfigUtil.interpolate(key.dup, envs)] = NginxConfigUtil.interpolate(value.to_s, envs)
       end
       break
     end
