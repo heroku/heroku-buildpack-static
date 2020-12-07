66779cfa80880ffff682978004df41bac4ddea8c
Sean Bird <sean@neiybor.com>
2020-12-07 12:04:33 -0700

    add ability to hide headers

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -40,6 +40,7 @@ class NginxConfig
       cleaned_path.chop! if cleaned_path.end_with?('/')
       json['proxies'][loc]['path'] = cleaned_path
       json['proxies'][loc]['host'] = uri.dup.tap { |u| u.path = '' }.to_s
+      json['proxies'][loc]['hide_headers'] = hash['hideHeaders'] || []
       %w[http https].each do |scheme|
         json['proxies'][loc]["redirect_#{scheme}"] = uri.dup.tap { |u| u.scheme = scheme }.to_s
         unless uri.to_s.end_with?('/')
