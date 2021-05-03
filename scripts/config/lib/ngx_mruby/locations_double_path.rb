a0d40a01057e207026d10888b4b80278e613b09e
Brayden <brayden.gibbons66@gmail.com>
2021-05-03 16:22:52 -0600

    add locations redirect for dlps

---
+++ scripts/config/lib/ngx_mruby/locations_double_path.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# ghetto require, since mruby doesn't have require
+eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
+
+USER_CONFIG = '/app/static.json'
+
+req         = Nginx::Request.new
+uri         = req.var.uri
+
+p uri
+city, state, type, subtype = uri.match(%r{\/([A-z\-']*)--([A-z\-']*)--([A-z\-]*)\/*([A-z]*)$}i).captures
+p city, state, type, subtype
+
+"#{state.downcase}/#{city.downcase}/#{type.downcase}#{ "/#{subtype}" unless subtype.empty? }"
---
+++ scripts/config/lib/ngx_mruby/locations_single_path.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# ghetto require, since mruby doesn't have require
+eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
+
+USER_CONFIG = '/app/static.json'
+
+req         = Nginx::Request.new
+uri         = req.var.uri
+
+city, state, type = uri.match(%r{\/([A-z\-']*)--([A-z\-']*)-(garages|long-term-parking|monthly-parking|self-storage|driveway-parking)$}i).captures
+p city,state,type
+
+
+"#{state.downcase}/#{city.downcase}/#{type.downcase}"
