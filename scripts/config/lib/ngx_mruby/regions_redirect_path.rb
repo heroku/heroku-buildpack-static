f67933e2967866b187dc470ad53a8991b1ddc65b
haotsai101 <tsaizhihao@gmail.com>
2022-01-13 16:01:58 -0700

    Add regions redirect rule

---
+++ scripts/config/lib/ngx_mruby/regions_redirect_path.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# ghetto require, since mruby doesn't have require
+eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
+
+USER_CONFIG = '/app/static.json'
+
+req         = Nginx::Request.new
+uri         = req.var.uri
+
+if uri.include?("indoor") || uri.include?("outdoor") || uri.include?("covered")
+    state, type, subtype = uri.match(%r{/regions/([A-z\-]*)\/([A-z\-]*)\/([A-z\-]*)$}mi).captures
+    "#{type.downcase}-near-me/#{subtype}/#{state}"
+else
+    state, type = uri.match(%r{/regions/([A-z\-]*)\/([A-z\-]*)$}mi).captures
+    "#{type.downcase}-near-me/#{state}"
+end
