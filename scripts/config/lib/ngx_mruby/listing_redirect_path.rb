2f57a122a93685b544a2eeef899fa874992b642e
haotsai101 <tsaizhihao@gmail.com>
2022-02-03 17:25:24 -0700

    Add back missing file

---
+++ scripts/config/lib/ngx_mruby/listing_redirect_path.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# ghetto require, since mruby doesn't have require
+eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
+
+USER_CONFIG = '/app/static.json'
+
+req         = Nginx::Request.new
+uri         = req.var.uri
+
+city, state, id = uri.match(%r{/listings/([A-z0-9\-]*)\/([A-z0-9\-]*)\/([0-9]*)$}mi).captures
+
+"#{state.downcase}/#{city.downcase}/#{id.downcase}"
