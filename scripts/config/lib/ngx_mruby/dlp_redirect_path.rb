caea0b78e29bdc218175781fbfd23fb0bebbddac
Alec Ostheimer <alecostheimer94@gmail.com>
2022-12-02 12:33:44 -0700

    added upper-case redirecting

---
+++ scripts/config/lib/ngx_mruby/dlp_redirect_path.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# ghetto require, since mruby doesn't have require
+eval(File.read('/app/bin/config/lib/nginx_config_util.rb'))
+
+USER_CONFIG = '/app/static.json'
+
+req         = Nginx::Request.new
+uri         = req.var.uri
+
+def format_url(uri)
+  type, subtype, state, city = uri.match(%r{/([A-z-]*)-near-me(?:/([a-z]*))?/([A-Z][A-z| |\-|%20]*)/([A-Z][A-z| |\-|%20]*)$}i).captures
+  state.gsub!(/%20| /, '-')
+  city.gsub!(/%20| /, '-')
+
+  "#{type.downcase}-near-me#{"/#{subtype.downcase}" unless subtype.nil?}/#{state.downcase}/#{city.downcase}"
+end
+
+format_url(uri)
